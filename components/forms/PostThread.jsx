0cf819d4b274e5bce446031c0c1e5b4e4ed486d7
NishitBaria <vinayakvispute262003@gmail.com>
2024-01-22 03:55:34 +0530

    Feat : Post Thread Form Implemented

---
+++ components/forms/PostThread.jsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import Image from "next/image";
+import { Textarea } from "@/components/ui/textarea";
+import { usePathname, useRouter } from "next/navigation";
+import { ThreadValidation } from "@/lib/validations/thread.validation";
+
+// import { updateUser } from "@/lib/actions/user.actions";
+
+function PostThread({ userId }) {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const form = useForm({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: "",
+    },
+  });
+
+  async function onSubmit(values) {}
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col justify-start gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full gap-3">
+              <FormLabel className="text-base-semibold text-light-2 mt-10">
+                Content
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                  rows={15}
+                  className="no-focus border border-dark-4 bg-dark-3 text-light-1 "
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="bg-primary-500">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export default PostThread;
